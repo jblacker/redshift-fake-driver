342db1be5b4774a74863b5b63b89ed67766fda16
Jordan Blacker <jblacker@crowdtwist.com>
2018-11-15 20:37:05 -0500

    Support $$ quoting in UNLOAD command parser

--- src/main/scala/jp/ne/opt/redshiftfake/parse/UnloadCommandParser.scala
+++ src/main/scala/jp/ne/opt/redshiftfake/parse/UnloadCommandParser.scala
@@ -11,7 +11,9 @@ class UnloadCommandParser extends BaseParser with QueryCompatibility {
       val offset = in.offset
       val spaceSkipped = handleWhiteSpace(source, offset)
 
-      if (source.length() > spaceSkipped + 2 && source.subSequence(spaceSkipped, spaceSkipped + 2) == "('") {
+      if (source.length() > spaceSkipped + 2 &&
+        (source.subSequence(spaceSkipped, spaceSkipped + 2) == "('" ||
+         source.subSequence(spaceSkipped, spaceSkipped + 3) == "($$" )) {
         val start = spaceSkipped + 2
 
         var i = start
@@ -19,13 +21,15 @@ class UnloadCommandParser extends BaseParser with QueryCompatibility {
         var escape: Option[Char] = None
         var prev2: Option[Char] = None
         var prev: Option[Char] = None
-        while (i < source.length() && !(escape != Some('\\') && prev2 == Some('\'') && prev == Some(')'))) {
+        while (i < source.length() && !(escape != Some('\\') &&
+            (prev2 == Some('\'') || prev2 == Some('$')) && prev == Some(')'))) {
           prev = Some(source.charAt(i))
           if (i > 0) { prev2 = Some(source.charAt(i - 1)) }
           if (i > 1) { escape = Some(source.charAt(i - 2)) }
           i += 1
         }
-        if (source.length() > 2 && source.subSequence(i - 2, i) == "')") {
+        if (source.length() > 2 &&
+            (source.subSequence(i - 2, i) == "')" || source.subSequence(i - 2, i) == "$)")) {
           Success(source.subSequence(start, i - 2).toString, in.drop(i - offset))
         } else {
           Failure("failed", in.drop(start - offset))
